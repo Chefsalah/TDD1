f0bd13876a72f715d02b643f8d991176b59c2e25
Chefsalah <71771220+Chefsalah@users.noreply.github.com>
2021-11-20 01:26:25 +0100

    Add files via upload

---
+++ FuhrplanVerwaltung/Auto.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FuhrplanVerwaltung
+{
+    public class Auto
+    {
+        private int kilometerstand;
+        private double verbrauchInLiternpro100K;
+        private double tankinhalt = 100;
+
+
+
+        public Auto(int kilometerstand)
+        {
+            this.kilometerstand = kilometerstand;
+        }
+        public Auto(int kilometerstand, double verbrauchInLiternpro100K, double tankinhalt) : this(kilometerstand)
+        {
+            this.verbrauchInLiternpro100K = verbrauchInLiternpro100K;
+            this.tankinhalt = tankinhalt;
+        }
+        public void Fahren(int streckeInKilometer)
+        {
+            if (streckeInKilometer < 1)
+            {
+                throw new ArgumentOutOfRangeException();
+
+            }
+            else
+            {
+                kilometerstand += streckeInKilometer;
+                Tankinhaltberechenen(streckeInKilometer);
+            }
+           
+        }
+        public int Kilometerstand
+        { get => kilometerstand; }
+        private void Tankinhaltberechenen(double streckeInKilometer)
+        {
+            double ergebniss = streckeInKilometer / 100 * verbrauchInLiternpro100K;
+            if (ergebniss>=tankinhalt)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            else
+            {
+                tankinhalt = tankinhalt - ergebniss;
+            }
+            
+
+        }
+
+        public double Tankinhalt2
+        {
+
+            get => tankinhalt;
+
+        }
+
+
+
+    }
+}
---
+++ FuhrplanverwaltungTests/AutoTests.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using FuhrplanVerwaltung;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FuhrplanverwaltungTests
+{
+    [TestClass]
+    public class Autotests
+    {
+        //1
+        [TestMethod]
+        public void Fahren_steigertJikometerstand()
+        {
+            //Arrange
+            int kilometerstand = 0;
+            Auto a = new Auto(kilometerstand);
+            int streckeInKilometern = 50;
+
+            //Act
+            a.Fahren(streckeInKilometern);
+
+            //Assert
+            Assert.AreEqual(50, a.Kilometerstand);
+
+
+        }
+        //2
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void Fahren_NichtNegativ()
+        {
+
+            //Arrange
+            int kilometerstand = 0;
+            Auto a = new Auto(kilometerstand);
+            int streckeinKilometer = -1;
+
+            //act
+            a.Fahren(streckeinKilometer);
+
+        }
+        //3
+
+        [TestMethod]
+        public void Tankinhalt_senken()
+        {
+            //Arrange
+            int kilometerstand = 100;
+            double Tankinhalt = 10;
+            double verbrauchInLiternpro100K = 5.7;
+            Auto a = new Auto(kilometerstand,verbrauchInLiternpro100K,Tankinhalt);
+            int streckeInKilometern = 100;
+            
+            //act
+            a.Fahren(streckeInKilometern);
+
+            Assert.AreEqual(4.3,a.Tankinhalt2);
+
+
+        }
+        //4
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void Fahren_Tankinhaltnichtüberschreiten()
+        {
+
+
+            //Arrange 
+            int kilometerstand = 100;
+            double Tankinhalt = 10;
+            double verbrauchInLiternpro100K = 10;
+            Auto a = new Auto(kilometerstand, verbrauchInLiternpro100K, Tankinhalt);
+            int streckeInKilometern = 200;
+
+            //Act
+            a.Fahren(streckeInKilometern);
+
+
+
+        }
+        //5
+    }
+}
